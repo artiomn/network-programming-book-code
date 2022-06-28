92eee439ad99e197d94683c3a43c7e8ede984dcd
Artiom N <artiom@artiom>
2022-06-28 13:58:10 +0300

    Multithreaded server was fixed

--- src/l4/mt_server/main.cpp
+++ src/l4/mt_server/main.cpp
@@ -22,6 +22,7 @@ extern "C"
 #   include <cwctype>
 #endif
 
+#include <socket_wrapper/socket_functions.h>
 #include <socket_wrapper/socket_headers.h>
 #include <socket_wrapper/socket_wrapper.h>
 #include <socket_wrapper/socket_class.h>
@@ -43,40 +44,6 @@ const wchar_t separ = fs::path::preferred_separator;
 const wchar_t separ = *reinterpret_cast<const wchar_t*>(&fs::path::preferred_separator);
 #endif
 
-std::unique_ptr<addrinfo, decltype(&freeaddrinfo)>
-get_serv_info(const char *port)
-{
-    struct addrinfo hints =
-    {
-        .ai_flags = AI_PASSIVE,
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-        .ai_protocol = IPPROTO_TCP
-    };
-    struct addrinfo *s_i;
-    int ai_status;
-
-    if ((ai_status = getaddrinfo(nullptr, port, &hints, &s_i)) != 0)
-    {
-        std::cerr << "getaddrinfo error " << gai_strerror(ai_status) << std::endl;
-        return std::unique_ptr<addrinfo, decltype(&freeaddrinfo)>(nullptr, freeaddrinfo);
-    }
-
-    return std::unique_ptr<addrinfo, decltype(&freeaddrinfo)>(s_i, freeaddrinfo);
-}
-
-
-void set_reuse_addr(socket_wrapper::Socket &sock)
-{
-    const int flag = 1;
-
-    // Allow reuse of port.
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&flag), sizeof(flag)) < 0)
-    {
-        throw std::logic_error("Set SO_REUSEADDR error");
-    }
-}
-
 
 socket_wrapper::Socket accept_client(socket_wrapper::Socket &server_sock)
 {
@@ -93,8 +60,9 @@ socket_wrapper::Socket accept_client(socket_wrapper::Socket &server_sock)
 
     assert(sizeof(sockaddr_in) == client_addr_length);
 
-    std::cout <<
-        "Client from " << inet_ntop(AF_INET, &(reinterpret_cast<const sockaddr_in * const>(&client_addr)->sin_addr), &addr[0], addr.size())
+    std::cout
+        << "Client from "
+        << inet_ntop(AF_INET, &(reinterpret_cast<const sockaddr_in * const>(&client_addr)->sin_addr), &addr[0], addr.size())
         << "..."
         << std::endl;
     return client_sock;
@@ -295,40 +263,33 @@ int main(int argc, const char * const argv[])
 
     try
     {
-        auto servinfo = get_serv_info(argv[1]);
-        if (!servinfo)
-        {
-            std::cerr << "Can't get servinfo!" << std::endl;
-            exit(EXIT_FAILURE);
-        }
+        auto servinfo = socket_wrapper::get_serv_info(argv[1]);
 
         socket_wrapper::Socket server_sock = {servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol};
 
         if (!server_sock)
         {
-            throw std::logic_error("Socket creation error");
+            throw std::logic_error("socket");
         }
 
         set_reuse_addr(server_sock);
 
         if (bind(server_sock, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
         {
-            throw std::logic_error("Bind error");
+            throw std::logic_error("bind");
         }
 
         std::list<std::future<bool>> pending_tasks;
+
+        if (listen(server_sock, clients_count) < 0)
+        {
+            throw std::logic_error("listen");
+        }
 
         std::cout
             << "Listening on port " << argv[1] << "...\n"
             << "Server path: " << fs::current_path()
             << std::endl;
-
-        if (listen(server_sock, clients_count) < 0)
-        {
-            throw std::logic_error("Listen error");
-        }
-
-        std::cout << "Listen was run..." << std::endl;
 
         while (true)
         {
@@ -336,7 +297,7 @@ int main(int argc, const char * const argv[])
 
             if (!client_sock)
             {
-                throw std::logic_error("Client socket error");
+                throw std::logic_error("Client socket accepting error: ");
             }
 
             pending_tasks.push_back(std::async(std::launch::async, [&](socket_wrapper::Socket &&sock)
@@ -371,7 +332,7 @@ int main(int argc, const char * const argv[])
     {
         std::cerr
             << e.what()
-            << " [" << sock_wrap.get_last_error_string() << "]!"
+            << ": " << sock_wrap.get_last_error_string() << "!"
             << std::endl;
         return EXIT_FAILURE;
     }
