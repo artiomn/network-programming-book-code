ccc503f3c16a4175bd5e2bc662a90263ef678a7a
Artiom N <artiom@artiom>
2023-01-06 21:00:44 +0300

    Simple Qt TCP server add

---
+++ src/ch12/cpp/qt-tcp-server/simple_variant.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <QObject>
+#include <QDebug>
+#include <QTcpSocket>
+#include <QTcpServer>
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        qDebug() << "Incorrect port!";
+        return EXIT_FAILURE;
+    }
+
+    auto port = std::stoi(argv[1]);
+
+    auto server = new QTcpServer();
+
+    // When user connects, signal will be emitted.
+    QObject::connect(server, &QTcpServer::newConnection, [server]()
+    {
+        QTcpSocket *client_socket = server->nextPendingConnection();
+
+        qDebug()
+            << "New connection from"
+            << client_socket->peerAddress() << ":"
+            << client_socket->peerPort();
+
+        client_socket->write("Qt TCP server\r\n");
+        client_socket->flush();
+
+        QObject::connect(client_socket, &QTcpSocket::readyRead, [client_socket]()
+        {
+            qDebug()
+               << "Client send data:"
+               << client_socket->readLine();
+        });
+
+        client_socket->waitForReadyRead(10000);
+
+        qDebug()
+            << "Close connection"
+            << client_socket->peerAddress() << ":"
+            << client_socket->peerPort();
+
+        client_socket->close();
+    });
+
+    if (!server->listen(QHostAddress::Any, port))
+    {
+        qDebug() << "Server could not start!";
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        qDebug() << "Server started on port" << port << "...";
+    }
+
+    while (true)
+    {
+        server->waitForNewConnection();
+    }
+}
