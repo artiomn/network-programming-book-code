02854fc13aa6676fa1c7ac771762abc11eb0282d
Artiom N <artiom@artiom>
2023-01-07 07:14:05 +0300

    Github build fix

--- src/docker_images/build_image/build.sh
+++ src/docker_images/build_image/build.sh
@@ -4,5 +4,8 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 IMAGE_TAG=artiomn/nb-build-image
 
 docker build -t "${IMAGE_TAG}" "${SCRIPT_PATH}" || exit 1
-[ "$1" == "-p" ] && docker push "${IMAGE_TAG}"
+
+if [ "$1" == "-p" ]; then
+    docker push "${IMAGE_TAG}"
+fi
 
