45656f97d79c2183e420373e5b61980367d3fd6d
Artiom N <artiom@artiom>
2023-01-07 20:52:04 +0300

    Ninja generator support add to build

--- build.sh
+++ build.sh
@@ -6,5 +6,12 @@ export CXX=g++
 #export CC=clang
 #export CXX=clang++
 
-cmake -DCMAKE_BUILD_TYPE=Debug -B build src
+ADD_OPTS=""
+
+if builtin type -P ninja &>/dev/null; then
+    echo "Using Ninja build..."
+    ADD_OPTS="${ADD_OPTS} -GNinja"
+fi
+
+cmake ${ADD_OPTS} -DCMAKE_BUILD_TYPE=Debug -B build src
 VERBOSE=1 cmake --build build --parallel  # --clean-first
