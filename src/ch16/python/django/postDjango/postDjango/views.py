9b494180b0eb13aa59ac31ab8ca60cee25383ede
Aleksei Barinov <F:\Mail>
2023-02-25 15:39:34 +0300

    add django examle

---
+++ src/ch16/python/django/postDjango/postDjango/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+      return render(request, 'index.html')
+def validate(request):
+   if request.method == 'POST':
+      username= request.POST["user"]
+      password = request.POST["pass"]
+      dict = {
+         'username': username,
+         'password': password
+      }
+      return render(request, 'validate.html', dict)
