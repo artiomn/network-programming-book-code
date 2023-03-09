a8ca63600e4099e825558f1c27e5b425113f3ef0
Artiom N <artiom@artiom>
2023-03-09 03:17:16 +0300

    Style fix

--- src/ch16/python/django/postDjango/postDjango/views.py
+++ src/ch16/python/django/postDjango/postDjango/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 def index(request):
       return render(request, 'index.html')
+
+
 def validate(request):
    if request.method == 'POST':
       username= request.POST["user"]
