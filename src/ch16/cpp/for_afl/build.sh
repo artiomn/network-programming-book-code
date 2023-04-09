a4e6d44bb34848fff1f4e228dfc4576e29297047
Artiom N <artiom@artiom>
2023-04-10 02:31:31 +0300

    AFL example fixed

--- src/ch16/cpp/for_afl/build.sh
+++ src/ch16/cpp/for_afl/build.sh
@@ -4,6 +4,4 @@
 #export CXX=afl-clang-fast++
 
 cmake -DCMAKE_C_COMPILER=afl-cc -DCMAKE_CXX_COMPILER=afl-c++ -B build .
-cmake -B build .
 VERBOSE=1 cmake --build build --parallel  # --clean-first
-
