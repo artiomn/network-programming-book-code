240cf54edd4e10c68c7fd987ea3ab494ae59708b
Artiom N <artiom@artiom>
2023-05-29 03:59:57 +0300

    MPI example fixed

--- src/ch20/cpp/boost_mpi_example/reduce.cpp
+++ src/ch20/cpp/boost_mpi_example/reduce.cpp
@@ -1,6 +1,7 @@
-#include <boost/mpi.hpp>
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+
+#include <boost/mpi.hpp>
 
 
 namespace mpi = boost::mpi;
@@ -15,13 +16,13 @@ int main()
 
     int my_number = std::rand();
 
+    std::cout << "  Process " << world.rank() << " value = " << my_number << std::endl;
+
     if (0 == world.rank())
     {
         int minimum;
         reduce(world, my_number, minimum, mpi::minimum<int>(), 0);
-        std::cout
-            << "The minimum value is " << minimum
-            << std::endl;
+        std::cout << "\nThe minimum value is " << minimum << std::endl;
     }
     else
     {
