190dad0b067d511daf416fb3194bc1500e3f1305
Artiom N <artiom@artiom>
2023-05-31 15:19:38 +0300

    Error processing in UPD server added

--- src/ch02/cpp/udp_server/main.cpp
+++ src/ch02/cpp/udp_server/main.cpp
@@ -1,16 +1,16 @@
+#include <socket_wrapper/socket_class.h>
+#include <socket_wrapper/socket_headers.h>
+#include <socket_wrapper/socket_wrapper.h>
+
 #include <algorithm>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <string>
 
-#include <socket_wrapper/socket_headers.h>
-#include <socket_wrapper/socket_wrapper.h>
-#include <socket_wrapper/socket_class.h>
-
 
 // Trim from end (in place).
-static inline std::string& rtrim(std::string& s)
+static inline std::string &rtrim(std::string &s)
 {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base(), s.end());
     return s;
@@ -19,7 +19,6 @@ static inline std::string& rtrim(std::string& s)
 
 int main(int argc, char const *argv[])
 {
-
     if (argc != 2)
     {
         std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
@@ -27,7 +26,7 @@ int main(int argc, char const *argv[])
     }
 
     socket_wrapper::SocketWrapper sock_wrap;
-    const int port { std::stoi(argv[1]) };
+    const int port{std::stoi(argv[1])};
 
     socket_wrapper::Socket sock = {AF_INET, SOCK_DGRAM, IPPROTO_UDP};
 
@@ -47,7 +46,7 @@ int main(int argc, char const *argv[])
 
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sock, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr)) != 0)
+    if (bind(sock, reinterpret_cast<const sockaddr *>(&addr), sizeof(addr)) != 0)
     {
         std::cerr << sock_wrap.get_last_error_string() << std::endl;
         // Socket will be closed in the Socket destructor.
@@ -67,39 +66,34 @@ int main(int argc, char const *argv[])
     while (true)
     {
         // Read content into buffer from an incoming client.
-        recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
-                            reinterpret_cast<sockaddr *>(&client_address),
-                            &client_address_len);
+        recv_len = recvfrom(
+            sock, buffer, sizeof(buffer) - 1, 0, reinterpret_cast<sockaddr *>(&client_address), &client_address_len);
 
         if (recv_len > 0)
         {
             buffer[recv_len] = '\0';
-            std::cout
-                << "Client with address "
-                << inet_ntop(AF_INET, &client_address.sin_addr, client_address_buf, sizeof(client_address_buf) / sizeof(client_address_buf[0]))
-                << ":" << ntohs(client_address.sin_port)
-                << " sent datagram "
-                << "[length = "
-                << recv_len
-                << "]:\n'''\n"
-                << buffer
-                << "\n'''"
-                << std::endl;
-            //if ("exit" == command_string) run = false;
-            //send(sock, &buf, readden, 0);
+            std::cout << "Client with address "
+                      << inet_ntop(
+                             AF_INET, &client_address.sin_addr, client_address_buf,
+                             sizeof(client_address_buf) / sizeof(client_address_buf[0]))
+                      << ":" << ntohs(client_address.sin_port) << " sent datagram "
+                      << "[length = " << recv_len << "]:\n'''\n"
+                      << buffer << "\n'''" << std::endl;
+            // if ("exit" == command_string) run = false;
+            // send(sock, &buf, readden, 0);
 
-/*            std::string command_string = {buffer, 0, len};
-            rtrim(command_string);
-            std::cout << command_string << std::endl;
-*/
+            /*            std::string command_string = {buffer, 0, len};
+                        rtrim(command_string);
+                        std::cout << command_string << std::endl;
+            */
             // Send same content back to the client ("echo").
-            sendto(sock, buffer, recv_len, 0, reinterpret_cast<const sockaddr *>(&client_address),
-                   client_address_len);
+            sendto(sock, buffer, recv_len, 0, reinterpret_cast<const sockaddr *>(&client_address), client_address_len);
         }
+        else if (recv_len < 0)
+            perror("recvfrom");
 
         std::cout << std::endl;
     }
 
     return EXIT_SUCCESS;
 }
-
