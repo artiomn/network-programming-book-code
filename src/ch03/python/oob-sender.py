09bbbb097eb52c73fb786beb4ced3fec7d418993
Artiom N <artiom@artiom>
2023-06-02 02:13:22 +0300

    Python OOB sender added

---
+++ src/ch03/python/oob-sender.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+sock.connect(('127.0.0.1', 12345))
+sock.send(b'abcd', socket.MSG_OOB)
