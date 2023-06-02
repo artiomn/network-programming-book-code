f83421c049324ea5038827fc5bad21d242eb634b
Artiom N <artiom@artiom>
2023-06-02 22:01:50 +0300

    Python dual-stack server added

---
+++ src/ch03/python/dualstack-server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import socket
+
+with socket.socket(socket.AF_INET6, socket.SOCK_STREAM, socket.IPPROTO_TCP) as sock:
+    sock.bind(('::', 12345, 0, 0))
+    sock.listen()
+    print(f'Listening socket = {sock}, ' f'dual stack = {socket.has_dualstack_ipv6()}\n')
+
+    while True:
+        s, addr = sock.accept()
+        print(f'Client socket = {s}\nClient address = {addr}')
