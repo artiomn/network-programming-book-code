a6a245a70c39275ac50de5d22acf0c6a786104bf
Artiom N <artiom@artiom>
2023-06-02 22:11:21 +0300

    Secondary dual-stack example added

---
+++ src/ch03/python/dualstack-server-2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import socket
+
+addr = ('', 12345)
+
+if socket.has_dualstack_ipv6():
+    s = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
+else:
+    s = socket.create_server(addr)
