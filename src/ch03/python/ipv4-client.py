586e5342aefa84e2fa353ed4dc9e2aa1270f1f95
Artiom N <artiom@artiom>
2023-06-02 22:04:34 +0300

    Python clients added

---
+++ src/ch03/python/ipv4-client.py
@@ -0,0 +1,6 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+sock.connect(('127.0.0.1', 12345))
+sock.send(b'test ipv4')
+sock.close()
---
+++ src/ch03/python/ipv6-client.py
@@ -0,0 +1,6 @@
+import socket
+
+sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+sock.connect(('::', 12345))
+sock.send(b'test ipv6')
+sock.close()
