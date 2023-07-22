5cfd0c1b483fe7c31047f0aa60545b3f2f2ce120
Artiom N <artiom@artiom>
2023-07-22 15:33:37 +0300

    Proxy server fix

--- src/ch07/cpp/simple_http_proxy/proxy_server.h
+++ src/ch07/cpp/simple_http_proxy/proxy_server.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <regex>
-#include <tuple>
-#include <thread>
-#include <iostream>
-
-#include <socket_wrapper/socket_headers.h>
 #include <socket_wrapper/socket_class.h>
+#include <socket_wrapper/socket_headers.h>
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <thread>
+#include <tuple>
 
 
 class ProxyServer final
@@ -14,13 +15,15 @@ class ProxyServer final
 public:
     static const std::regex uri_regexp;
 
+    // cppcheck-suppress unusedStructMember
     const size_t back_log = 10;
-    const unsigned short default_target_port = 80;
+    // cppcheck-suppress unusedStructMember
+    const unsigned short default_target_port = 80;  // NOLINT
 
-    typedef std::tuple<std::string, std::string, unsigned short> uri_data;
+    typedef std::tuple<std::string, std::string, unsigned short> uri_data;  // NOLINT
 
 public:
-    ProxyServer(unsigned short port = 8080);
+    explicit ProxyServer(unsigned short port = 8080);
     ~ProxyServer();
 
 public:
@@ -31,8 +34,9 @@ public:
     std::string read_line(socket_wrapper::Socket &sock) const;
 
     // Send HTML error message to the client.
-    void client_error(socket_wrapper::Socket &sock, const std::string &cause, int err_num, const std::string &short_message,
-                      const std::string &long_message) const;
+    void client_error(
+        socket_wrapper::Socket &sock, const std::string &cause, int err_num, const std::string &short_message,
+        const std::string &long_message) const;
     // Extract the host name, path to the resource and the port number from the URL.
     uri_data parse_uri(const std::string &uri) const;
 
@@ -41,27 +45,26 @@ public:
      * header to send to the host that the client is trying to access.
      * Ensures that the headers follow the writup specification.
      * If the host header is present in the reqest headers frm the client it copies
-     * it directly. Else it generates it using the hostname and adds it. 
+     * it directly. Else it generates it using the hostname and adds it.
      */
     std::tuple<std::string, std::string> parse_request_headers(socket_wrapper::Socket &s) const;
 
-    /*     Core part of the proxy server that parses the request from the client, 
-            forwards it to the server and sends the response (cached or real-time)  
+    /*     Core part of the proxy server that parses the request from the client,
+            forwards it to the server and sends the response (cached or real-time)
             back to the client.
         The proxy is LRU cache enabled with synchronization.
     */
     void proxify(socket_wrapper::Socket client_socket);
 
-    unsigned short get_port() const { return port_; }
+    unsigned short get_port() const { return port_; }  // NOLINT
 
 private:
-    bool try_to_connect(socket_wrapper::Socket &s, const sockaddr* sa, size_t sa_size);
+    bool try_to_connect(socket_wrapper::Socket &s, const sockaddr *sa, size_t sa_size);
     socket_wrapper::Socket connect_to_target_server(const std::string &host_name, unsigned short port);
 
 private:
-    unsigned short port_;
+    unsigned short port_;  // NOLINT
     bool started_ = false;
     socket_wrapper::SocketWrapper sock_wrap_;
     socket_wrapper::Socket sock_;
 };
-
