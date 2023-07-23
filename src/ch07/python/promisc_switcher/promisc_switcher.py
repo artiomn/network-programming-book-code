8d82a47a1857a8495f990fb21e39b3db6640aa1f
Artiom N <artiom@artiom>
2023-07-23 19:56:04 +0300

    Chapter 7 Python examples added

---
+++ src/ch07/python/promisc_switcher/promisc_switcher.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import ctypes
+from fcntl import ioctl
+import socket
+import sys
+from typing import Optional
+
+
+class InterfacePromiscSwitcher(ctypes.Structure):
+    IFF_PROMISC = 0x100
+    SIOCGIFFLAGS = 0x8913
+    SIOCSIFFLAGS = 0x8914
+
+    _fields_ = [('ifr_ifrn', ctypes.c_char * 16), ('ifr_flags', ctypes.c_short)]
+
+    def __init__(self, if_name: str, *args, **kw):
+        super().__init__(*args, **kw)
+        self._sock: socket.socket
+        if 'socket' in kw:
+            self._sock = kw['socket']
+        else:
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        self.ifr_ifrn = if_name.encode()
+
+    @property
+    def socket(self) -> Optional[socket.socket]:
+        return self._sock
+
+    def set_promisc(self):
+        sock = self._sock
+        if sys.platform == 'win32':
+            sock.bind((self.ifr_ifrn, 0))
+            sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+        else:
+            ioctl(sock.fileno(), self.SIOCGIFFLAGS, self)
+            self.ifr_flags |= self.IFF_PROMISC  # pylint: disable=E1101(no-member)
+            ioctl(sock.fileno(), self.SIOCSIFFLAGS, self)
+
+    def unset_promisc(self, close_socket: bool = True):
+        if sys.platform == 'win32':
+            self._sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        else:
+            self.ifr_flags &= ~self.IFF_PROMISC  # pylint: disable=E1101(no-member)
+            ioctl(self._sock.fileno(), self.SIOCSIFFLAGS, self)
+        if close_socket:
+            self._sock.close()
+
+    def __enter__(self):
+        self.set_promisc()
+        return self
+
+    def __exit__(self, *args):
+        self.unset_promisc()
+
+
+if '__main__' == __name__:
+    from os import popen
+    from sys import argv
+
+    if len(argv) != 2:
+        print(f'{argv[0]} <interface name>')
+        sys.exit(1)
+
+    interface_name = argv[1]
+
+    with InterfacePromiscSwitcher(interface_name) as ifreq:
+        print(f'Promiscuous mode for the "{interface_name}" enabled:')
+        print(popen(f'ip a show {interface_name}').readline())  # nosec B605
+
+    print(f'Promiscuous mode for the "{interface_name}" disabled:')
+    print(popen(f'ip a show {interface_name}').readline())  # nosec B605
---
+++ src/ch07/python/python-sniffer.py
@@ -0,0 +1,369 @@
+#!/usr/bin/python
+
+import os
+import struct
+import binascii
+import socket
+from typing import Any, Dict, List, Set
+
+
+import sys
+from abc import abstractmethod, ABCMeta
+
+from promisc_switcher.promisc_switcher import InterfacePromiscSwitcher
+
+
+ETH_P_IP = 0x0800
+ETH_P_ALL = 0x0003
+SO_BINDTODEVICE = 25
+RECV_BUF_SIZE = 65535
+
+
+class NetworkProtocolHeaderType(ABCMeta):
+    channel_protocols: Set['NetworkProtocolHeader'] = set()
+    protocols: Dict[int, 'NetworkProtocolHeader'] = {}
+
+    def __new__(cls, name, bases, *args, **kwargs):
+        c = super().__new__(cls, name, bases, *args, **kwargs)
+
+        if name == 'NetworkProtocolHeader':
+            return c
+
+        # pylint: disable=E1101(no-member)
+        c.header_size: int = struct.calcsize(c.format_string)
+        try:
+            proto_code = c.protocol_code
+            proto_class = NetworkProtocolHeaderType.protocols.get(proto_code)
+
+            if proto_class is not None:
+                raise KeyError(f'Class for the protocol code {proto_code} already' f'exists [{proto_class.__name__}]!')
+            NetworkProtocolHeaderType.protocols[proto_code] = c
+        except AttributeError:
+            NetworkProtocolHeaderType.channel_protocols.add(c)
+
+        return c
+
+
+class NetworkProtocolHeader(metaclass=NetworkProtocolHeaderType):
+    def __init__(self, packet):
+        self._packet = packet
+
+    @property
+    def fields(self):
+        # pylint: disable=E1101(no-member)
+        return struct.unpack(self.format_string, self._packet[: self.header_size])
+
+    @property
+    @abstractmethod
+    def inner_protocol_code(self) -> int:
+        """Upper-layer protocol code."""
+
+    @property
+    @abstractmethod
+    def payload_offset(self) -> int:
+        """Upper-layer header offset."""
+
+    @classmethod
+    def unpack_packet(cls, base_protocol, packet, skip_channel_proto_check: bool = False) -> List[Any]:
+        if not isinstance(base_protocol, NetworkProtocolHeaderType):
+            raise SyntaxError(f'{base_protocol!r} is not a descendant class of the ' f'{cls.__class__.__name__}!')
+
+        if not skip_channel_proto_check and base_protocol not in cls.channel_protocols:
+            raise LookupError(f'{base_protocol.__name__} is not a channel ' f'protocol class!')
+
+        headers = []
+
+        cur_header = base_protocol(packet)
+        headers.append(cur_header)
+
+        while True:
+            packet = packet[cur_header.payload_offset :]
+            try:
+                proto_header_class = cls.protocols.get(cur_header.inner_protocol_code)
+                if proto_header_class is None:
+                    break
+
+                cur_header = proto_header_class(packet)  # type: ignore
+
+                headers.append(cur_header)
+            except NotImplementedError:
+                break
+
+        return headers
+
+
+class EthernetHeader(NetworkProtocolHeader):
+    # "!" - unpacking from the network byte order.
+    format_string = '!6s6sH'
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        self.destination_mac_address, self.source_mac_address, self.protocol = self.fields
+        self.header_size: int
+
+    @property
+    def inner_protocol_code(self) -> int:
+        return int(self.protocol)
+
+    @property
+    def payload_offset(self) -> int:
+        # pylint: disable=E1101(no-member)
+        return self.header_size
+
+    @staticmethod
+    def print_mac(addr):
+        pa = binascii.hexlify(addr).decode()
+        return ':'.join([pa[i : i + 2] for i in range(0, len(pa), 2)])
+
+    def __str__(self):
+        return (
+            f'[{self.__class__.__name__}]:\n'
+            f'  Src MAC: {self.print_mac(self.destination_mac_address)}\n'
+            f'  Dst MAC: {self.print_mac(self.source_mac_address)}\n'
+            f'  EtherType: 0x{self.protocol:04x}'
+        )
+
+
+# pylint: disable=R0902(too-many-instance-attributes)
+class Ipv4Header(NetworkProtocolHeader):
+    protocol_code = 0x0800
+    format_string = '!BBHHHBBH4s4s'
+    word_length = 4
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        (
+            self.ip_version,
+            self.tos,
+            self.total_length,
+            self.identification,
+            self.fragment_offset,
+            self.ttl,
+            self.protocol,
+            self.header_checksum,
+            self.source_address,
+            self.destination_address,
+        ) = self.fields
+        self.ihl = self.ip_version & 0x0F
+        self.ip_version &= 0xF0
+        self.ip_version >>= 4
+
+        self.header_size: int
+        self.protocol = int(self.protocol)
+
+        assert self.ip_version == 4
+        assert self.ihl * self.word_length >= self.header_size  # pylint: disable=E1101(no-member)
+
+    @property
+    def inner_protocol_code(self) -> int:
+        return int(self.protocol)
+
+    @property
+    def payload_offset(self) -> int:
+        return int(self.ihl * self.word_length)
+
+    def __str__(self):
+        spacing = ' ' * 6
+        return (
+            f'    [{self.__class__.__name__}]:\n'
+            f'{spacing}Payload Off: {self.payload_offset}\n'
+            f'{spacing}TOS: {self.tos}\n'
+            f'{spacing}TTL: {self.ttl}\n'
+            f'{spacing}Proto: {self.protocol}\n'
+            f'{spacing}Length: {self.total_length}\n'
+            f'{spacing}Src IP: '
+            f'{socket.inet_ntop(socket.AF_INET, self.source_address)}\n'
+            f'{spacing}Dst IP: '
+            f'{socket.inet_ntop(socket.AF_INET, self.destination_address)}'
+        )
+
+
+class Ipv6Header(NetworkProtocolHeader):
+    # From the EtherType list.
+    protocol_code = 0x86DD
+    format_string = '!BBHHBB16s16s'
+
+    word_length = 4
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        # pylint: disable=R0902(too-many-instance-attributes)
+        (
+            first_byte,
+            second_byte,
+            flow_label_lsb,
+            self.payload_length,
+            self.next_header,
+            self.hop_limit,
+            self.source_address,
+            self.destination_address,
+        ) = self.fields
+
+        self.ip_version = (first_byte & 0xF0) >> 4
+        self.traffic_class = ((first_byte & 0x0F) << 4) | ((second_byte & 0xF0) >> 4)
+        self.flow_label = (second_byte & 0x0F << 16) | flow_label_lsb
+        self.header_size: int
+
+        assert self.ip_version == 6
+
+    @property
+    def inner_protocol_code(self) -> int:
+        return int(self.next_header)
+
+    @property
+    def payload_offset(self) -> int:
+        # pylint: disable=E1101(no-member)
+        return int(self.header_size)
+
+    def __str__(self):
+        spacing = ' ' * 6
+        return (
+            f'    [{self.__class__.__name__}]:\n'
+            f'{spacing}Traffic Class: '
+            f'{self.traffic_class}\n'
+            f'{spacing}Flow Label: {self.flow_label}\n'
+            f'{spacing}Next Header: {self.next_header}\n'
+            f'{spacing}Payload Length: '
+            f'{self.payload_length}\n'
+            f'{spacing}Src IP: '
+            f'{socket.inet_ntop(socket.AF_INET6, self.source_address)}\n'
+            f'{spacing}Dst IP: '
+            f'{socket.inet_ntop(socket.AF_INET6, self.destination_address)}'
+        )
+
+
+class IcmpHeader(NetworkProtocolHeader):
+    # From the IANA list.
+    protocol_code = 1
+    format_string = '!BBH'
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        self.message_type, self.code, self.checksum = self.fields
+        self.header_size: int
+
+    @property
+    def inner_protocol_code(self) -> int:
+        raise NotImplementedError('ICMP has not inner protocol!')
+
+    @property
+    def payload_offset(self) -> int:
+        # pylint: disable=E1101(no-member)
+        return int(self.header_size)
+
+    def __str__(self):
+        spacing = ' ' * 10
+        return (
+            f'        [{self.__class__.__name__}]:\n'
+            f'{spacing}Message Type: {self.message_type}\n'
+            f'{spacing}Code: {self.code}\n'
+            f'{spacing}Checksum: 0x{self.checksum:04x}'
+        )
+
+
+class Icmp6Header(IcmpHeader):
+    protocol_code = 58
+
+    @property
+    def inner_protocol_code(self) -> int:
+        raise NotImplementedError('ICMP6 has not inner protocol!')
+
+
+class TcpHeader(NetworkProtocolHeader):
+    protocol_code = 6
+    format_string = '!HHLLBBHHH'
+    word_length = 4
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        (
+            self.source_port,
+            self.destination_port,
+            self.sequence_number,
+            self.acknowledge_number,
+            offset_reserved,
+            self.tcp_flag,
+            self.window,
+            self.checksum,
+            self.urgent_pointer,
+        ) = self.fields
+
+        self.data_offset = ((offset_reserved & 0xF0) >> 4) * self.word_length
+
+    @property
+    def inner_protocol_code(self) -> int:
+        raise NotImplementedError('TCP has arbitrary user-defined inner protocol!')
+
+    @property
+    def payload_offset(self) -> int:
+        return int(self.data_offset)
+
+    def __str__(self):
+        spacing = ' ' * 10
+        return (
+            f'        [{self.__class__.__name__}]:\n'
+            f'{spacing}Src Port: {self.source_port}\n'
+            f'{spacing}Dst Port: {self.destination_port}\n'
+            f'{spacing}Seq Number: {self.sequence_number}\n'
+            f'{spacing}Ack Number: {self.acknowledge_number}\n'
+            f'{spacing}Data Off: {self.data_offset}\n'
+            f'{spacing}Flags: 0x{self.tcp_flag:04x}\n'
+            f'{spacing}Window: {self.window}\n'
+            f'{spacing}Urgent Ptr: {self.urgent_pointer}\n'
+            f'{spacing}Checksum: 0x{self.checksum:04x}'
+        )
+
+
+class UdpHeader(NetworkProtocolHeader):
+    # Номер из списка IANA.
+    protocol_code = 17
+    format_string = '!HHHH'
+
+    def __init__(self, packet):
+        super().__init__(packet)
+        self.source_port, self.destination_port, self.length, self.checksum = self.fields
+        self.header_size: int
+
+    @property
+    def inner_protocol_code(self) -> int:
+        raise NotImplementedError('UDP has arbitrary user-defined inner protocol!')
+
+    @property
+    def payload_offset(self) -> int:
+        # pylint: disable=E1101(no-member)
+        return int(self.header_size)
+
+    def __str__(self):
+        spacing = ' ' * 10
+        return (
+            f'        [{self.__class__.__name__}]:\n'
+            f'{spacing}Src Port: {self.source_port}\n'
+            f'{spacing}Dst Port: {self.destination_port}\n'
+            f'{spacing}Length: {self.length}\n'
+            f'{spacing}Checksum: 0x{self.checksum:04x}'
+        )
+
+
+if len(sys.argv) != 2:
+    print(f'{sys.argv[0]} <interface name>')
+    sys.exit(1)
+
+interface_name = sys.argv[1]
+
+with InterfacePromiscSwitcher(interface_name) as ips:
+    if 'win32' == os.name:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+        sock.setsockopt(socket.SOL_SOCKET, SO_BINDTODEVICE, interface_name.encode())
+
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    else:
+        sock = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
+
+    sock.bind((interface_name, 0))
+
+    while True:
+        data = sock.recvfrom(RECV_BUF_SIZE)
+        for p in NetworkProtocolHeader.unpack_packet(EthernetHeader, data[0]):
+            print(p)
+        print()
