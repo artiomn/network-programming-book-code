16c6310fe56bced6ad04c03cada9597b2037561d
Artiom N <artiom@artiom>
2023-08-15 01:22:31 +0300

    Select server fixed

--- src/ch11/cpp/select_aio_server/main.cpp
+++ src/ch11/cpp/select_aio_server/main.cpp
@@ -5,34 +5,33 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <optional>
 #include <list>
+#include <optional>
 #include <vector>
 
 #if !defined(_WIN32)
 extern "C"
 {
-#   include <signal.h>
-#   include <netinet/tcp.h>
-#   include <sys/ioctl.h>
+#    include <netinet/tcp.h>
+#    include <signal.h>
+#    include <sys/ioctl.h>
 }
 #else
-#   include <cwctype>
 extern "C"
 {
-#   include <io.h>
+#    include <io.h>
 }
+#    include <cwctype>
 
-#define ioctl (ioctlsocket)
+#    define ioctl (ioctlsocket)
 
 #endif
 
 #include <fcntl.h>
-
-#include <socket_wrapper/socket_headers.h>
-#include <socket_wrapper/socket_wrapper.h>
 #include <socket_wrapper/socket_class.h>
 #include <socket_wrapper/socket_functions.h>
+#include <socket_wrapper/socket_headers.h>
+#include <socket_wrapper/socket_wrapper.h>
 
 
 const auto clients_count = 10;
@@ -42,15 +41,15 @@ namespace fs = std::filesystem;
 
 
 #if !defined(MAX_PATH)
-#   define MAX_PATH (256)
+#    define MAX_PATH (256)
 #endif
 
 #if defined(_WIN32)
-#   define close (_close)
-#   define read (_read)
+#    define close (_close)
+#    define read (_read)
 const wchar_t separ = fs::path::preferred_separator;
 #else
-const wchar_t separ = *reinterpret_cast<const wchar_t*>(&fs::path::preferred_separator);
+const wchar_t separ = *reinterpret_cast<const wchar_t *>(&fs::path::preferred_separator);
 #endif
 
 
@@ -58,7 +57,7 @@ void set_nonblock(SocketDescriptorType fd)
 {
     const IoctlType flag = 1;
 
-    if (ioctl(fd, FIONBIO, const_cast<IoctlType*>(&flag)) < 0)
+    if (ioctl(fd, FIONBIO, const_cast<IoctlType *>(&flag)) < 0)
     {
         throw std::system_error(errno, std::system_category(), "Setting non-blocking mode");
     }
@@ -78,9 +77,12 @@ public:
 
 
 public:
-    Transceiver(socket_wrapper::Socket &&client_sock) : buffer_(buffer_size), client_sock_(std::move(client_sock)) {}
-    Transceiver(Transceiver&& t) : buffer_(buffer_size), client_sock_(std::move(t.client_sock_)) {}
-    Transceiver(const Transceiver&) = delete;
+    explicit Transceiver(socket_wrapper::Socket &&client_sock)
+        : buffer_(buffer_size), client_sock_(std::move(client_sock))
+    {
+    }
+    Transceiver(Transceiver &&t) : buffer_(buffer_size), client_sock_(std::move(t.client_sock_)) {}
+    Transceiver(const Transceiver &) = delete;
     Transceiver() = delete;
     ~Transceiver()
     {
@@ -90,16 +92,22 @@ public:
 public:
     const socket_wrapper::Socket &ts_socket() const { return client_sock_; }
     int file_descriptor() const { return file_descriptor_; }
+    bool read_finished() const { return read_finished_; }
+    size_t sent_size() const { return sent_size_; }
 
 public:
     IOStatus send_buffer()
     {
         assert(true == static_cast<bool>(client_sock_));
-        if (buffer_index_ <= 0) return IOStatus::no_data;
+        if (send_index_ == read_index_) return read_finished() ? IOStatus::finished : IOStatus::no_data;
+
+        auto data_size = ((send_index_ > read_index_) ? buffer_.size() : read_index_) - send_index_;
 
         while (true)
         {
-            auto result = send(client_sock_, buffer_.data(), buffer_index_, 0);
+            auto result = send(client_sock_, &buffer_[send_index_], data_size, 0);
+            std::cout << data_size << " bytes requested, " << result << " bytes was sent" << std::endl;
+
             if (-1 == result)
             {
                 if (need_to_repeat()) continue;
@@ -108,7 +116,9 @@ public:
 
             if (!result) return IOStatus::finished;
 
-            buffer_index_ -= result;
+            sent_size_ += result;
+            send_index_ = (send_index_ + result) % buffer_.size();
+
             break;
         }
 
@@ -117,29 +127,42 @@ public:
 
     IOStatus read_data()
     {
-        if ((-1 == file_descriptor_) || (buffer_index_ >= buffer_.size())) return IOStatus::error;
+        if (read_finished_) return IOStatus::finished;
+        if (-1 == file_descriptor_) return IOStatus::error;
 
-        std::cout << "Reading data..." << std::endl;
+        auto data_size = ((send_index_ <= read_index_) ? buffer_.size() - !send_index_ : send_index_ - 1) - read_index_;
+
+        // Buffer is full.
+        if (!data_size) return IOStatus::no_data;
 
         while (true)
         {
-            auto result = read(file_descriptor_, buffer_.data() + buffer_index_, buffer_.size() - buffer_index_);
+            auto result = read(file_descriptor_, &buffer_[read_index_], data_size);
+            std::cout << data_size << " bytes requested, " << result << " bytes was read" << std::endl;
+
             if (-1 == result)
             {
                 if (need_to_repeat()) continue;
                 return IOStatus::error;
             }
 
-            if (!result) return IOStatus::finished;
+            if (0 == result)
+            {
+                read_finished_ = true;
+                // close(file_descriptor_);
+                // file_descriptor_ = -1;
+                return IOStatus::finished;
+            }
 
-            buffer_index_ += result;
+            read_index_ = (read_index_ + result) % buffer_.size();
+
             break;
         }
 
         return IOStatus::ok;
     }
 
-    int open_file(fs::path const& file_path)
+    int open_file(fs::path const &file_path)
     {
         std::cout << "Opening file " << file_path << "..." << std::endl;
 #if !defined(_WIN32)
@@ -167,19 +190,19 @@ public:
             if (-1 == result)
             {
                 if (need_to_repeat()) continue;
-                throw std::logic_error("Socket reading error");
+                throw std::system_error(errno, std::system_category(), "Socket reading error");
             }
 
             auto fragment_begin = buffer.begin() + recv_bytes;
-            auto ret_iter = std::find_if(fragment_begin, fragment_begin + result,
-                                         [](char sym) { return '\n' == sym || '\r' == sym;  });
+            auto ret_iter = std::find_if(
+                fragment_begin, fragment_begin + result, [](char sym) { return '\n' == sym || '\r' == sym; });
             if (ret_iter != buffer.end())
             {
                 *ret_iter = '\0';
                 recv_bytes += std::distance(fragment_begin, ret_iter);
                 break;
             }
-            recv_bytes += result;
+            recv_bytes += result % buffer_.size();
             if (size == recv_bytes) break;
         }
 
@@ -198,7 +221,7 @@ private:
         {
             case EINTR:
             case EAGAIN:
-            // case EWOULDBLOCK: // EWOULDBLOCK == EINTR.
+                // case EWOULDBLOCK: // EWOULDBLOCK == EINTR.
                 return true;
         }
 
@@ -207,40 +230,56 @@ private:
 
 private:
     std::vector<char> buffer_;
-    size_t buffer_index_ = 0;
+    size_t read_index_ = 0;
+    size_t send_index_ = 0;
     int file_descriptor_ = -1;
     socket_wrapper::Socket client_sock_;
+    bool read_finished_ = false;
+    size_t sent_size_ = 0;
 };
 
 
 class Client
 {
 public:
-    Client(socket_wrapper::Socket &&sock) :
-        tsr_(std::move(sock))
+    explicit Client(socket_wrapper::Socket &&sock) : tsr_(std::move(sock))
     {
-        std::cout
-            << "Client ["<< static_cast<int>(tsr_.ts_socket()) << "] "
-            << "was created..."
-            << std::endl;
+        std::cout << "Client [" << static_cast<int>(tsr_.ts_socket()) << "] "
+                  << "was created..." << std::endl;
     }
 
-    Client(Client &&client) : tsr_(std::move(client.tsr_)) { }
+    Client(Client &&client) : tsr_(std::move(client.tsr_)) {}
 
     ~Client()
     {
-        std::cout
-            << "Client ["<< static_cast<int>(tsr_.ts_socket()) << "] "
-            << "removing..."
-            << std::endl;
+        std::cout << "Client [" << static_cast<int>(tsr_.ts_socket()) << "] "
+                  << "removing..." << std::endl;
     }
 
 public:
     operator SocketDescriptorType() const { return static_cast<SocketDescriptorType>(tsr_.ts_socket()); }
-    int get_fd() const { return tsr_.file_descriptor();  }
+    int get_fd() const { return tsr_.file_descriptor(); }
     bool file_opened() const { return get_fd() != -1; }
+    const fs::path &get_file_path() const { return file_path_; }
+    bool read_finished() const { return tsr_.read_finished(); }
+    size_t sent_size() const { return tsr_.sent_size(); }
 
 public:
+    int update_file_path()
+    {
+        auto fp = recv_file_path();
+
+        if (file_opened() || (std::nullopt == fp)) return -1;
+        file_path_ = *fp;
+
+        return open_file(file_path_);
+    }
+
+    Transceiver::IOStatus read_data() { return tsr_.read_data(); }
+
+    Transceiver::IOStatus send_data() { return tsr_.send_buffer(); }
+
+private:
     std::optional<fs::path> recv_file_path()
     {
         auto request_data = tsr_.get_request();
@@ -250,12 +289,9 @@ public:
         auto file_path = fs::weakly_canonical(request_data).wstring();
 
 #if defined(_WIN32)
-        std::transform(cur_path.begin(), cur_path.end(), cur_path.begin(),
-            [](wchar_t c) { return std::towlower(c); }
-        );
-        std::transform(file_path.begin(), file_path.end(), file_path.begin(),
-            [](wchar_t c) { return std::towlower(c); }
-        );
+        std::transform(cur_path.begin(), cur_path.end(), cur_path.begin(), [](wchar_t c) { return std::towlower(c); });
+        std::transform(
+            file_path.begin(), file_path.end(), file_path.begin(), [](wchar_t c) { return std::towlower(c); });
 #endif
 
         if (file_path.find(cur_path) == 0)
@@ -268,40 +304,18 @@ public:
 
     int open_file(const fs::path &file_path)
     {
-        if (!(fs::exists(file_path) && fs::is_regular_file(file_path))) return false;
+        if (!(fs::exists(file_path) && fs::is_regular_file(file_path))) return -1;
 
         return tsr_.open_file(file_path);
     }
 
-    Transceiver::IOStatus read_data()
-    {
-        return tsr_.read_data();
-    }
-
-    Transceiver::IOStatus send_data()
-    {
-        return tsr_.send_buffer();
-    }
-
-public:
-    int update_file_path()
-    {
-        if (file_opened()) return -1;
-
-        file_path_ = recv_file_path();
-
-        if (std::nullopt == file_path_) return -1;
-
-        return open_file(*file_path_);
-    }
-
 private:
     Transceiver tsr_;
-    std::optional<fs::path> file_path_;
+    fs::path file_path_;
 };
 
 
-template<size_t max_clients_count = clients_count>
+template <size_t max_clients_count = clients_count>
 class SelectProcessor
 {
 public:
@@ -309,8 +323,7 @@ public:
     const std::string max_clients_msg = "Maximum clients limit exceeds!";
 
 public:
-    SelectProcessor(socket_wrapper::Socket &server) :
-        server_socket_(server), max_available_descriptor_(server)
+    explicit SelectProcessor(socket_wrapper::Socket &server) : server_socket_(server), max_available_descriptor_(server)
     {
     }
 
@@ -328,16 +341,19 @@ private:
 
         for (auto &c : clients_)
         {
-            FD_SET(static_cast<SocketDescriptorType>(c), &read_descriptors_set_);
+            if (!c.read_finished())
+            {
+                FD_SET(static_cast<SocketDescriptorType>(c), &read_descriptors_set_);
+            }
             FD_SET(static_cast<SocketDescriptorType>(c), &write_descriptors_set_);
             FD_SET(static_cast<SocketDescriptorType>(c), &err_descriptors_set_);
 
 #if !defined(WIN32)
             // Windows select() doesn't support file descriptors: it works only with sockets.
-            auto fd = c.get_fd();
 
-            if (fd != -1)
+            if (c.file_opened() && !c.read_finished())
             {
+                auto fd = c.get_fd();
                 FD_SET(static_cast<int>(fd), &read_descriptors_set_);
                 FD_SET(static_cast<int>(fd), &err_descriptors_set_);
                 if (fd > max_available_descriptor_) max_available_descriptor_ = fd;
@@ -353,10 +369,7 @@ private:
 
         if (clients_.size() >= max_clients)
         {
-            std::cout
-                << "Server can't accept new client (max = "
-                << max_clients << ")"
-                << std::endl;
+            std::cout << "Server can't accept new client (max = " << max_clients << ")" << std::endl;
             send(client_sock, max_clients_msg.c_str(), max_clients_msg.size(), 0);
             return;
         }
@@ -364,22 +377,19 @@ private:
         // Set non-blocking mode.
         set_nonblock(client_sock);
 
-        std::cout
-            << "Client " << clients_.size() << " added."
-            << std::endl;
+        std::cout << "Client " << clients_.size() << " added." << std::endl;
         clients_.emplace_back(Client(std::move(client_sock)));
     }
 
 public:
     void run_step()
     {
-        timeval timeout = { .tv_sec = 1, .tv_usec = 000 };
+        timeval timeout = {.tv_sec = 1, .tv_usec = 0};
         build_sock_list();
 
-        auto active_descriptors = select(max_available_descriptor_ + 1,
-            &read_descriptors_set_,
-            &write_descriptors_set_,
-            &err_descriptors_set_, &timeout);
+        auto active_descriptors = select(
+            max_available_descriptor_ + 1, &read_descriptors_set_, &write_descriptors_set_, &err_descriptors_set_,
+            &timeout);
 
         if (active_descriptors < 0)
         {
@@ -402,7 +412,7 @@ public:
             {
                 // Client socket error.
                 // remove_descriptor(*client_iter);
-                std::cout << "Socket error: " << *client_iter << std::endl;
+                std::cerr << "Socket error: " << *client_iter << std::endl;
                 clients_.erase(client_iter++);
                 continue;
             }
@@ -410,7 +420,7 @@ public:
             if (FD_ISSET(client_iter->get_fd(), &err_descriptors_set_))
             {
                 // Client file error.
-                std::cout << "File error: " << client_iter->get_fd() << std::endl;
+                std::cerr << "File error: " << client_iter->get_fd() << std::endl;
                 clients_.erase(client_iter++);
                 continue;
             }
@@ -418,7 +428,12 @@ public:
             if (FD_ISSET(static_cast<SocketDescriptorType>(*client_iter), &read_descriptors_set_))
             {
                 std::cout << "Socket ready for reading: " << *client_iter << std::endl;
-                client_iter->update_file_path();
+                if (-1 == client_iter->update_file_path())
+                {
+                    std::cerr << "File [re]opening error: " << client_iter->get_fd() << std::endl;
+                    clients_.erase(client_iter++);
+                    continue;
+                }
             }
 
             try
@@ -433,15 +448,34 @@ public:
                 if (FD_ISSET(static_cast<SocketDescriptorType>(client), &write_descriptors_set_))
                 {
                     std::cout << "Socket ready for writing: " << client << std::endl;
-                    if (Transceiver::IOStatus::error == client.send_data()) throw std::logic_error("send data to socket");
+                    switch (client.send_data())
+                    {
+                        case Transceiver::IOStatus::error:
+                        {
+                            std::cerr << "Data sending error: " << *client_iter << std::endl;
+                            clients_.erase(client_iter++);
+                            continue;
+                        }
+                        case Transceiver::IOStatus::finished:
+                        {
+                            if (client.read_finished())
+                            {
+                                std::cout << "File " << client.get_file_path()
+                                          << " was sent [size = " << client.sent_size() << " bytes]" << std::endl;
+                                // Client disconnected: all file was sent.
+                                clients_.erase(client_iter++);
+                                continue;
+                            }
+                        }
+                    }
                 }
 
                 if (FD_ISSET(client.get_fd(), &read_descriptors_set_))
                 {
                     std::cout << "File ready for reading: " << client.get_fd() << std::endl;
-                    if (Transceiver::IOStatus::finished == client.read_data())
+                    if (Transceiver::IOStatus::error == client.read_data())
                     {
-                        // Client disconnected: all file was sent.
+                        std::cerr << "File reading error: " << *client_iter << std::endl;
                         clients_.erase(client_iter++);
                         continue;
                     }
@@ -450,27 +484,17 @@ public:
                 ++client_iter;
                 continue;
             }
-            catch(const std::logic_error &e)
+            catch (const std::exception &e)
             {
                 std::cerr << "Error: " << e.what() << std::endl;
-                // remove_descriptor(*client_iter);
                 clients_.erase(client_iter++);
             }
-            catch(...)
+            catch (...)
             {
                 std::cerr << "Unknown client error!" << std::endl;
-                // remove_descriptor(*client_iter);
                 clients_.erase(client_iter++);
             }
         }
-    }
-
-private:
-    void remove_descriptor(int descr)
-    {
-        FD_CLR(descr, &read_descriptors_set_);
-        FD_CLR(descr, &write_descriptors_set_);
-        FD_CLR(descr, &err_descriptors_set_);
     }
 
 private:
@@ -483,7 +507,7 @@ private:
 };
 
 
-int main(int argc, const char * const argv[])
+int main(int argc, const char *const argv[])
 {
     if (argc != 2)
     {
@@ -510,25 +534,23 @@ int main(int argc, const char * const argv[])
 
         if (!server_sock)
         {
-            throw std::logic_error("socket");
+            throw std::system_error(errno, std::system_category(), "server socket");
         }
 
         set_reuse_addr(server_sock);
 
         if (bind(server_sock, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
         {
-            throw std::logic_error("bind");
+            throw std::system_error(errno, std::system_category(), "bind");
         }
 
         if (listen(server_sock, clients_count) < 0)
         {
-            throw std::logic_error("Listen error");
+            throw std::system_error(errno, std::system_category(), "listen");
         }
 
-        std::cout
-            << "Listening on port " << argv[1] << "...\n"
-            << "Server path: " << fs::current_path()
-            << std::endl;
+        std::cout << "Listening on port " << argv[1] << "...\n"
+                  << "Server path: " << fs::current_path() << std::endl;
 
         SelectProcessor sp(server_sock);
 
@@ -537,14 +559,11 @@ int main(int argc, const char * const argv[])
             sp.run_step();
         }
 
-        std::cout<<"\n\nExiting normally\n";
+        std::cout << "\n\nExiting normally\n";
     }
     catch (const std::exception &e)
     {
-        std::cerr
-            << e.what()
-            << ": " << sock_wrap.get_last_error_string() << "!"
-            << std::endl;
+        std::cerr << e.what() << ": " << sock_wrap.get_last_error_string() << "!" << std::endl;
         return EXIT_FAILURE;
     }
 
