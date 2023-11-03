399ff1d4a0d19aab5afc4843603460805506ad1d
Artiom N. <artiom@artiom>
2023-11-04 00:37:30 +0300

    Thrift2 example fix

--- src/ch14/python/thrift/thriftpy2_example/client.py
+++ src/ch14/python/thrift/thriftpy2_example/client.py
@@ -6,4 +6,5 @@ from thriftpy2.rpc import make_client
 
 phones_thrift = thriftpy2.load(str(get_scheme_file()), module_name='phones_thrift')
 client = make_client(phones_thrift.PhoneService, '127.0.0.1', 9090)
+
 print(client.findAll(), client.findById(112))
