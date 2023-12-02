46b50fd3708ec6efe6102ea023224ce3a5f54c5f
Artiom N. <artiom@artiom>
2023-12-03 01:41:00 +0300

    Oatpp example style fix

--- src/ch17/cpp/oatpp-example/test/tests.cpp
+++ src/ch17/cpp/oatpp-example/test/tests.cpp
@@ -1,27 +1,30 @@
 
-#include "MyControllerTest.hpp"
-
 #include <iostream>
 
-void runTests() {
-  OATPP_RUN_TEST(MyControllerTest);
+#include "MyControllerTest.hpp"
+
+void runTests()
+{
+    OATPP_RUN_TEST(MyControllerTest);
 }
 
-int main() {
 
-  oatpp::base::Environment::init();
+int main()
+{
+    oatpp::base::Environment::init();
 
-  runTests();
+    runTests();
 
-  /* Print how much objects were created during app running, and what have left-probably leaked */
-  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
-  std::cout << "\nEnvironment:\n";
-  std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
-  std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
+    /* Print how much objects were created during app running, and what have left-probably leaked */
+    /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better
+     * performance */
+    std::cout << "\nEnvironment:\n";
+    std::cout << "objectsCount = " << oatpp::base::Environment::getObjectsCount() << "\n";
+    std::cout << "objectsCreated = " << oatpp::base::Environment::getObjectsCreated() << "\n\n";
 
-  OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
+    OATPP_ASSERT(oatpp::base::Environment::getObjectsCount() == 0);
 
-  oatpp::base::Environment::destroy();
+    oatpp::base::Environment::destroy();
 
-  return 0;
+    return 0;
 }
