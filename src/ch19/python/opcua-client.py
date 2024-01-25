10925540ebda366707744e00346840aa594e8296
Artiom N. <artiom@artiom>
2024-01-25 17:55:51 +0300

    OPC UA examples fix

--- src/ch19/python/opcua-client.py
+++ src/ch19/python/opcua-client.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-from time import strftime, sleep, gmtime
+from time import sleep, ctime
+from typing import Tuple
 from opcua import Client
 
 
-URL = "opc.tcp://localhost:4840"
+def measure_value(m_var, mes_time: float, delay: float = 0.4) -> Tuple[float, float]:
+    m_var_time = m_var.get_child('2:Time')
 
-with Client(URL) as client:
+    while (last_mes_time := m_var_time.get_value()) <= mes_time:
+        sleep(delay)
+
+    return m_var.get_value(), last_mes_time
+
+
+with Client('opc.tcp://localhost:4840') as client:
     client.connect()
     root = client.nodes.root
     multimeter = root.get_child(['Objects', '2:Multimeter'])  # or client.get_node('ns=2;i=2')
@@ -15,17 +23,19 @@ with Client(URL) as client:
 
     print(f'Root = {root}, multimeter = {multimeter}')
 
+    voltage = voltmeter.get_child('2:Voltage')
+    mes_v_time = 0.0
+    current = ammeter.get_child('2:Current')
+    mes_c_time = 0.0
+
     while True:
         m_mode = multimeter.call_method('2:SetMode', 'VOLTAGE')
-        voltage = voltmeter.get_child('2:Voltage').get_value()
-        mes_v_time = voltmeter.get_child('2:Time').get_value()
-        disp_v_time = strftime('%Y/%M/%D %H:%M:%S', gmtime(mes_v_time))
-        print(f'Mode = {m_mode}, value = {voltage} V, mes. time = {disp_v_time}')
+        # m_mode = multimeter.get_child('2:SetMode').set_value('VOLTAGE')
+        voltage_value, mes_v_time = measure_value(voltage, mes_v_time)
+        print(f'Mode = {m_mode}, value = {voltage_value} V, mes. time = {ctime(mes_v_time)}')
 
         m_mode = multimeter.call_method('2:SetMode', 'CURRENT')
+        current_value, mes_c_time = measure_value(current, mes_c_time)
 
-        current = ammeter.get_child('2:Current').get_value()
-        mes_a_time = voltmeter.get_child('2:Time').get_value()
-        disp_a_time = strftime('%Y/%M/%D %H:%M:%S', gmtime(mes_a_time))
-        print(f'Mode = {m_mode}, value = {current} A, mes. time = {disp_a_time}')
+        print(f'Mode = {m_mode}, value = {current_value} A, mes. time = {ctime(mes_c_time)}')
         sleep(1)
--- src/ch19/python/opcua-server.py
+++ src/ch19/python/opcua-server.py
@@ -4,7 +4,9 @@ import asyncio
 from enum import Enum
 from time import time
 import random
-from asyncua import Server, uamethod, ua
+from typing import Union
+from asyncua import Server, uamethod
+from asyncua.common.structures104 import new_enum
 
 
 class Multimeter:
@@ -20,7 +22,7 @@ class Multimeter:
         return self._mode
 
     @mode.setter
-    def mode(self, mode: 'Multimeter.Mode'):
+    def mode(self, mode: Union['Multimeter.Mode', str]):
         if isinstance(mode, str):
             mode = Multimeter.Mode[mode]
         elif not isinstance(mode, self.Mode):
@@ -37,46 +39,42 @@ class Multimeter:
 
 
 class MMSwitcher:
-    def __init__(self, device):
-        self._device = device
+    def __init__(self, mode_var):
+        self._mode_var = mode_var
 
     @uamethod
-    def __call__(self, parent, d_mode):
-        self._device.mode = d_mode
-        print(f'New device mode = {self._device.mode}')
+    async def __call__(self, parent, d_mode):
+        print(f'New device mode = {d_mode}')
+        await self._mode_var.set_value(Multimeter.Mode[d_mode])
         return d_mode
 
 
-async def setup_multimeter(server, device):
+async def setup_multimeter(server: Server, device: Multimeter):
     ns = await server.register_namespace('Multimeter')
+
+    mode_enum_type = await new_enum(server, ns, 'MultimeterMode', [value.name for value in Multimeter.Mode])
+
     objects = server.get_objects_node()
-
-    t_enums = await server.get_root_node().get_child(['0:Types', '0:DataTypes', '0:BaseDataType', '0:Enumeration'])
-    mode_enum_type = await t_enums.add_data_type(ns, 'MultimeterMode')
-    es = await mode_enum_type.add_property(
-        0, 'EnumStrings', [ua.LocalizedText(value.name) for value in Multimeter.Mode]
-    )
-    await es.add_property(0, 'ValueRank', 1)
-    await es.add_property(0, 'ArrayDimensions', 1)
-
     multimeter = await objects.add_object(ns, 'Multimeter')
     await multimeter.add_property(ns, 'device_sn', '123-231')
 
     mode = await multimeter.add_variable(ns, 'Mode', device.mode, datatype=mode_enum_type.nodeid)
     await mode.set_writable()
 
-    await multimeter.add_method(ns, 'SetMode', MMSwitcher(device), [mode_enum_type.nodeid], [mode_enum_type.nodeid])
+    await multimeter.add_method(
+        ns, 'SetMode', MMSwitcher(mode).__call__, [mode_enum_type.nodeid], [mode_enum_type.nodeid]
+    )
 
     voltmeter = await multimeter.add_object(ns, 'Voltmeter')
     ammeter = await multimeter.add_object(ns, 'Ammeter')
 
     vm_voltage = await voltmeter.add_variable(ns, 'Voltage', 0.0)
-    vm_mes_time = await voltmeter.add_variable(ns, 'Time', time())
+    vm_mes_time = await vm_voltage.add_property(ns, 'Time', time())
 
     am_current = await ammeter.add_variable(ns, 'Current', 0.0)
-    am_mes_time = await ammeter.add_variable(ns, 'Time', time())
+    am_mes_time = await am_current.add_property(ns, 'Time', time())
 
-    return vm_voltage, vm_mes_time, am_current, am_mes_time
+    return vm_voltage, vm_mes_time, am_current, am_mes_time, mode
 
 
 async def main(server_url='opc.tcp://0.0.0.0:4840'):
@@ -86,13 +84,14 @@ async def main(server_url='opc.tcp://0.0.0.0:4840'):
     await server.init()
     server.set_endpoint(server_url)
     server.set_server_name('Test server')
+    vm_voltage, vm_mes_time, am_current, am_mes_time, mode = await setup_multimeter(server, multimeter_device)
 
     async with server:
-        vm_voltage, vm_mes_time, am_current, am_mes_time = await setup_multimeter(server, multimeter_device)
-
         while True:
             # Obtain information from the device.
+            multimeter_device.mode = await mode.get_value()
             print(f'Device mode = {multimeter_device.mode}')
+
             if Multimeter.Mode.VOLTAGE == multimeter_device.mode:
                 voltage = multimeter_device.measure()
                 print(f'Setting voltage = {voltage}')
