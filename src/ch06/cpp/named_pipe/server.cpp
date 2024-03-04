9c35ba40e5c7bd172cb048637fef5f1a6870316e
artiom <artiom@nomail.cpm>
2024-03-05 01:04:14 +0300

    Named pipe example fix

--- src/ch06/cpp/named_pipe/server.cpp
+++ src/ch06/cpp/named_pipe/server.cpp
@@ -1,26 +1,25 @@
 #include <windows.h>
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
     HANDLE h_pipe;
-    LPTSTR message = TEXT("Default message from client");
+    using tstring = std::basic_string<TCHAR, std::char_traits<TCHAR>, std::allocator<TCHAR>>;
+    tstring message = TEXT("I'm server: ");
+
     std::vector<TCHAR> read_buffer(512);
 
-    const LPTSTR pipename = TEXT("\\\\.\\pipe\\mynamedpipe");
-
-    if (argc > 1)
-    {
-        message = argv[1];
-    }
+    const tstring pipe_name = TEXT(R"(\\.\pipe\test_pipe)");
 
     // Try to open a named pipe; wait for it, if necessary.
     while (true)
     {
-        h_pipe = CreateFile(pipename, GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+        h_pipe = CreateNamedPipe(pipe_name.c_str(), PIPE_ACCESS_DUPLEX, PIPE_TYPE_MESSAGE | PIPE_WAIT, PIPE_UNLIMITED_INSTANCES,
+                                 message.size() * sizeof(TCHAR), read_buffer.size() * sizeof(TCHAR), NMPWAIT_USE_DEFAULT_WAIT, nullptr);
 
         if (h_pipe != INVALID_HANDLE_VALUE) break;
 
@@ -30,58 +29,85 @@ int main(int argc, const char **argv)
             std::cerr << "Could not open pipe." << std::endl;
             return EXIT_FAILURE;
         }
-
-        // All pipe instances are busy, so wait for 20 seconds.
-        if (!WaitNamedPipe(pipename, 20000))
-        {
-            std::cerr << "Could not open pipe" << std::endl;
-            return EXIT_FAILURE;
-        }
-    }
-
-    // The pipe connected; change to message-read mode.
-    DWORD mode = PIPE_READMODE_MESSAGE;
-    auto f_success = SetNamedPipeHandleState(h_pipe, &mode, nullptr, nullptr);
-
-    if (!f_success)
-    {
-        std::cerr << "SetNamedPipeHandleState() failed." << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    // Send a message to the pipe server and read the response.
-    f_success = TransactNamedPipe(
-        h_pipe, message, (lstrlen(message) + 1) * sizeof(TCHAR), read_buffer.data(),
-        read_buffer.size() * sizeof(read_buffer::value_type), nullptr, nullptr);
-
-    if (!f_success && (GetLastError() != ERROR_MORE_DATA))
-    {
-        std::cerr << "TransactNamedPipe failed." << std::endl;
-        return 0;
     }
 
     while (true)
     {
-        DWORD read_bytes;
-        std::cout << "Data: " << std::string(read_buffer.begin(), read_buffer.end()) << std::endl;
+        std::cout << "Waiting for the connections..." << std::endl;
 
-        // Break if TransactNamedPipe or ReadFile is successful
-        if (f_success) break;
+        if (!ConnectNamedPipe(h_pipe, nullptr))
+        {
+            std::cerr << "Could not connect pipe" << std::endl;
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
+
+        std::cout << "Pipe connected, reading data..." << std::endl;
+
+        // The pipe connected; change to message-read mode.
+        DWORD mode = PIPE_READMODE_MESSAGE;
+        if (!SetNamedPipeHandleState(h_pipe, &mode, nullptr, nullptr))
+        {
+            std::cerr << "SetNamedPipeHandleState() failed." << std::endl;
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
 
         // Read from the pipe if there is more data in the message.
-        f_success = ReadFile(
-            h_pipe, read_buffer.data(), read_buffer.size() * sizeof(read_buffer::value_type), &read_bytes, nullptr);
+        DWORD read_bytes;
 
         // Exit if an error other than ERROR_MORE_DATA occurs.
-        if (!f_success && (GetLastError() != ERROR_MORE_DATA))
-            break;
+        if (!ReadFile(h_pipe, read_buffer.data(), read_buffer.size() * sizeof(decltype(read_buffer)::value_type), &read_bytes, nullptr) && (GetLastError() != ERROR_MORE_DATA))
+        {
+            DisconnectNamedPipe(h_pipe);
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
         else
-            std::cout << "Data: " << std::string(read_buffer.begin(), read_buffer.end()) << std::endl;
+        {
+            std::cout << "Data read: " << std::string(read_buffer.begin(), read_buffer.begin() + read_bytes) << std::endl;
+        }
+
+        auto new_msg = message + tstring(read_buffer.begin(), read_buffer.begin() + read_bytes);
+
+        std::cout << "Sending data..." << std::endl;
+
+        DWORD written_bytes;
+        auto result = WriteFile(
+            h_pipe,
+            new_msg.c_str(),
+            new_msg.size() * sizeof(TCHAR),
+            &written_bytes,
+            nullptr
+        );
+
+        if (result)
+        {
+            std::cout << "Bytes sent: " << written_bytes << std::endl;
+        }
+        else
+        {
+            std::cout << "Failed to send data." << std::endl;
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
+
+        if (!FlushFileBuffers(h_pipe))
+        {
+            std::cerr << "FlushFileBuffers() failed." << std::endl;
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
+
+        if (!DisconnectNamedPipe(h_pipe))
+        {
+            std::cerr << "Could not connect pipe." << std::endl;
+            CloseHandle(h_pipe);
+            return EXIT_FAILURE;
+        }
     }
 
-    _getch();
-
-    CloseHandle(hPipe);
+    CloseHandle(h_pipe);
 
     return EXIT_SUCCESS;
 }
