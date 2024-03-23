5cdd11278e95529d8cb98e1030264b8f9beacbe4
artiom <artiom@nomail.cpm>
2024-03-23 20:31:08 +0300

    Windows notification example implemented

--- src/ch06/cpp/notify/main.cpp
+++ src/ch06/cpp/notify/main.cpp
@@ -1,11 +1,15 @@
 extern "C"
 {
-#include <iphlpapi.h>
-#include <windows.h>
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <ip2string.h>
+#include <mstcpip.h>
+#include <windows.h>
 }
 
 #include <iostream>
+#include <vector>
 
 
 int main()
@@ -22,14 +26,50 @@ int main()
         }
     }
 
+    MIB_IPFORWARDROW fr_row;
+
+    std::vector<MIB_IPADDRTABLE> mib_table;
+    ULONG mib_size = 0;
+    if (GetIpAddrTable(nullptr, &mib_size, false) != ERROR_INSUFFICIENT_BUFFER)
+    {
+        std::cerr << "Incorrect GetIpAddrTable() call!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    mib_table.resize(mib_size / sizeof(MIB_IPADDRTABLE) + 1);
+
+    if (GetIpAddrTable(mib_table.data(), &mib_size, false) != NO_ERROR)
+    {
+        std::cerr << "Incorrect GetIpAddrTable() call!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string ip_addr;
+    uint32_t my_ip_addr = 0;
+    ip_addr.resize(INET_ADDRSTRLEN);
+
+    for (size_t i = 0; i < mib_table[0].dwNumEntries; ++i)
+    {
+        auto row = mib_table[0].table[i];
+        if (INET_IS_ADDR_LOOPBACK(AF_INET, &row.dwAddr)) continue;
+        my_ip_addr = row.dwAddr;
+        std::cout << "My IP = " << inet_ntop(AF_INET, &row.dwAddr, ip_addr.data(), ip_addr.size()) << std::endl;
+    }
+
+    uint32_t google_dns_addr;
+    inet_pton(AF_INET, "8.8.8.8", &google_dns_addr);
+
+    if (NO_ERROR == GetBestRoute(google_dns_addr, my_ip_addr, &fr_row))
+    {
+        std::cout << "1. Iface index: " << fr_row.dwForwardIfIndex << " [" << inet_ntop(AF_INET, &fr_row.dwForwardNextHop, ip_addr.data(), ip_addr.size()) << "]" << std::endl;
+    }
+
     if (WAIT_OBJECT_0 == WaitForSingleObject(overlap.hEvent, INFINITE))
         std::cout << "Routing table changed." << std::endl;
 
-    MIB_IPFORWARDROW fr_row;
-
-    if (NO_ERROR == GetBestRoute(inet_addr("8.8.8.8"), inet_addr("127.0.0.1"), &fr_row))
+    if (NO_ERROR == GetBestRoute(google_dns_addr, my_ip_addr, &fr_row))
     {
-        std::cout << "Iface index: " << fr_row.dwForwardIfIndex << " [" << fr_row.dwForwardNextHop << "]" << std::endl;
+        std::cout << "2. Iface index: " << fr_row.dwForwardIfIndex << " [" << inet_ntop(AF_INET, &fr_row.dwForwardNextHop, ip_addr.data(), ip_addr.size()) << "]" << std::endl;
         return EXIT_SUCCESS;
     }
     return EXIT_FAILURE;
