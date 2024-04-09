89115b75269e3e0bf9e576fe9f6a1df4bb6d14aa
Artiom N. <artiom@artiom>
2024-04-09 20:51:45 +0300

    Flatbuffers scheme regenerated

--- src/ch14/cpp/flatbuffers/scheme_generated.h
+++ src/ch14/cpp/flatbuffers/scheme_generated.h
@@ -4,13 +4,12 @@
 #ifndef FLATBUFFERS_GENERATED_SCHEME_MYGAME_H_
 #define FLATBUFFERS_GENERATED_SCHEME_MYGAME_H_
 
-// cppcheck-suppress missingInclude
 #include "flatbuffers/flatbuffers.h"
 
 // Ensure the included flatbuffers.h is the same version as when this file was
 // generated, otherwise it may not be compatible.
 static_assert(
-    FLATBUFFERS_VERSION_MAJOR == 23 && FLATBUFFERS_VERSION_MINOR == 5 && FLATBUFFERS_VERSION_REVISION == 26,
+    FLATBUFFERS_VERSION_MAJOR == 24 && FLATBUFFERS_VERSION_MINOR == 3 && FLATBUFFERS_VERSION_REVISION == 25,
     "Non-compatible flatbuffers version included");
 
 namespace MyGame
