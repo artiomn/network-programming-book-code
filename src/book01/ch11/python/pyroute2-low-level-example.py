7f19048d781ba03e102d1701ce48835bcd364a34
Artiom N. <artiom@artiom>
2024-05-24 02:08:40 +0300

    Fix

--- src/book01/ch11/python/pyroute2-low-level-example.py
+++ src/book01/ch11/python/pyroute2-low-level-example.py
@@ -12,31 +12,30 @@ def print_if_attrs(if_attrs):
             print(f'Address: {attr_val}')
 
 
-ipr = IPRoute()
+with IPRoute() as ipr:
+    # lookup the index
+    devs = ipr.link_lookup(ifname=sys.argv[1])
 
-# lookup the index
-devs = ipr.link_lookup(ifname=sys.argv[1])
+    if not devs:
+        print(f'Device "{sys.argv[1]}" was not found!')
+        sys.exit(1)
 
-if not devs:
-    print(f'Device "{sys.argv[1]}" was not found!')
-    sys.exit(1)
+    dev_index = devs[0]
 
-dev_index = devs[0]
+    print(f'Devices count = {len(devs)}, Device index = {dev_index}')
 
-print(f'Devices count = {len(devs)}, Device index = {dev_index}')
+    print_if_attrs(ipr.link('get', index=dev_index)[0]['attrs'])
 
-print_if_attrs(ipr.link('get', index=dev_index)[0]['attrs'])
+    # Bring it down.
+    ipr.link('set', index=dev_index, state='down')
 
-# Bring it down.
-ipr.link('set', index=dev_index, state='down')
+    # Change the interface MAC address and rename it just for fun.
+    ipr.link('set', index=dev_index, address='00:11:22:33:44:55', ifname='eno2')
 
-# Change the interface MAC address and rename it just for fun.
-ipr.link('set', index=dev_index, address='00:11:22:33:44:55', ifname='eno2')
+    # Add primary IP address.
+    ipr.addr('add', index=dev_index, address='10.0.0.1', mask=24, broadcast='10.0.0.255')
 
-# Add primary IP address.
-ipr.addr('add', index=dev_index, address='10.0.0.1', mask=24, broadcast='10.0.0.255')
+    # Bring it up.
+    ipr.link('set', index=dev_index, state='up')
 
-# Bring it up.
-ipr.link('set', index=dev_index, state='up')
-
-print_if_attrs(ipr.link('get', index=dev_index)[0]['attrs'])
+    print_if_attrs(ipr.link('get', index=dev_index)[0]['attrs'])
