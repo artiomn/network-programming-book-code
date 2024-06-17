7e58c159b796670a2961a985574d277c0bf41162
Sergey Talantov <sergey.talantov@gmail.com>
2024-06-17 18:11:01 +0300

    Fix chapter 20

--- src/book01/ch20/cpp/winet/main.cpp
+++ src/book01/ch20/cpp/winet/main.cpp
@@ -6,35 +6,40 @@ extern "C"
 }
 
 
+#include <array>
 #include <iostream>
-#include <vector>
 
 
 int main()
 {
-    auto internet_handle = InternetOpen(TEXT("Mozilla/5.12345"), INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0);
-
-
-    if (internet_handle)
+    const auto internet_handle = InternetOpen(TEXT("Mozilla/5.12345"), INTERNET_OPEN_TYPE_PRECONFIG, nullptr, nullptr, 0);
+    if (!internet_handle)
     {
-        auto url_handle =
-            InternetOpenUrl(internet_handle, TEXT("http://artiomsoft.ru"), 0, 0, INTERNET_FLAG_RAW_DATA, 0);
-
-        if (url_handle)
-        {
-            std::vector<TCHAR> buf(1024);
-            DWORD read_bytes;
-
-            while (InternetReadFile(url_handle, buf.data(), buf.size(), &read_bytes))
-            {
-                if (!read_bytes) break;
-                std::ostream_iterator<TCHAR> out_it(std::cout, "");
-                std::copy(buf.begin(), buf.begin() + read_bytes, out_it);
-            }
-
-            InternetCloseHandle(url_handle);
-        }
-
-        InternetCloseHandle(internet_handle);
+        std::cerr << "InternetOpen failed with code " << GetLastError() << std::endl;
+        return EXIT_FAILURE;
     }
+
+    const auto url_handle =
+        InternetOpenUrl(internet_handle, TEXT("http://artiomsoft.ru"), 0, 0, INTERNET_FLAG_RAW_DATA, 0);
+
+    if (!url_handle)
+    {
+        std::cerr << "InternetOpenUrl failed with code " << GetLastError() << std::endl;
+        InternetCloseHandle(internet_handle);
+        return EXIT_FAILURE;
+    }
+
+    std::array<TCHAR, 1024> buf;
+    DWORD read_bytes = 0;
+
+    while (InternetReadFile(url_handle, buf.data(), static_cast<DWORD>(buf.size()), &read_bytes))
+    {
+        if (!read_bytes) break;
+        std::ostream_iterator<TCHAR> out_it(std::cout, "");
+        std::copy(buf.begin(), buf.begin() + read_bytes, out_it);
+    }
+
+    InternetCloseHandle(url_handle);
+    InternetCloseHandle(internet_handle);
+    return EXIT_SUCCESS;
 }
