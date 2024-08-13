39113cfade5f9a5407005ec770aca0d367710094
Artiom N. <artiom@artiom>
2024-08-14 01:45:56 +0300

    Chapter 2 example style fix

--- src/book01/ch02/python/ip-convert.py
+++ src/book01/ch02/python/ip-convert.py
@@ -3,8 +3,8 @@
 import socket
 
 
-ip4 = socket.inet_pton(socket.AF_INET, "10.12.110.57")
-ip6 = socket.inet_pton(socket.AF_INET6, "2001:db8:63b3:1::3490")
+ip4 = socket.inet_pton(socket.AF_INET, '10.12.110.57')
+ip6 = socket.inet_pton(socket.AF_INET6, '2001:db8:63b3:1::3490')
 
 print(socket.inet_ntop(socket.AF_INET, ip4))
 print(socket.inet_ntop(socket.AF_INET6, ip6))
